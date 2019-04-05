ba52fcf8294a9b9ac7cd8252369c6176cdbc7d10
eau <eau+code@unix4fun.net>
2019-04-05 10:29:55 +0200

    forgot error.go

---
+++ error.go
@@ -0,0 +1,16 @@
+package passwd
+
+// very good point and advice
+// https://dave.cheney.net/2016/04/07/constant-errors
+// also avoid your error checks can be diverted cross packages
+// when in usage in the rest of an package ecosystem
+
+type Error string
+
+func (e Error) Error() string { return string(e) }
+
+const (
+	ErrParse       = Error("parse error")
+	ErrUnsupported = Error("unsupported")
+	ErrMismatch    = Error("mismatch")
+)
